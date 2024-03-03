46a6fff8b3f02cea718eb1951e9f5974e0dec3c2
mk0vacev1c <m_k0vacev1c@hotmail.com>
2024-03-03 14:47:17 +0100

    refactoring all
    refactored all and patched v1

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,12 +1,12 @@
 export const darkTheme = {
-  bg: "#1C1C27",
+  bg: "#090917",
   bgLight: "#1C1E27",
-  primary: "#854CE6",
+  primary: "#51D791",
   text_primary: "#F2F3F4",
   text_secondary: "#b1b2b3",
   card: "#171721",
   card_light: "#191924",
-  button: "#854CE6",
+  button: "#51D791",
   white: "#FFFFFF",
   black: "#000000",
 };
@@ -18,5 +18,5 @@ export const lightTheme = {
   text_primary: "#111111",
   text_secondary: "#48494a",
   card: "#FFFFFF",
-  button: "#5c5b5b",
+  button: "#51D791",
 };
